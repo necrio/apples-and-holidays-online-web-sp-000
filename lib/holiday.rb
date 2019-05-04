a8ed7145ecead89d89c3825b722062410634c564
Keith Isham <kisham8899@gmail.com>
2019-05-04 13:36:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,7 +21,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 
   # return the second element in the 4th of July array
 
-    return holiday_hash[:summer][:fourth_of_july][1] 
+    return holiday_hash[:summer][:fourth_of_july][1]
 
 end
 

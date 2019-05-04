6bd547d4c446aec3f3803c49f8a60498d459220f
Keith Isham <kisham8899@gmail.com>
2019-05-04 13:37:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,6 +25,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 
 end
 
+ second_supply_for_fourth_of_july(holiday_hash)
+
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
